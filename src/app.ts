bd1c3823e6c5014b688222ee9c724e6e9c3edd6e
raulrsoares <raulrodriguessoares@gmail.com>
2025-04-12 03:29:18 -0300

    execution timer

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,14 @@
-import express from 'express';
+import express, { Express } from 'express';
 import morgan from 'morgan';
 import { router } from './routes/routes';
 import { errorHandler } from './middlewares/errorHandler';
 import { notFoundHandler } from './middlewares/notFound';
+import { timeLog } from './middlewares/timer.middlewares';
 
-const app = express();
+const app: Express = express();
 
 app.use(express.json());
+app.use(timeLog);
 app.use(morgan('tiny'));
 app.use('/api', router);
 app.use(notFoundHandler);
