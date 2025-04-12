5bc5573e50a1bb9038787d5a43d2fc4d79c04c77
raulrsoares <raulrodriguessoares@gmail.com>
2025-04-12 03:30:25 -0300

    remove comments

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import { router as authRoutes } from './auth.routes';
 import { router as customersRoutes } from './customers.route';
-// import { jwtAuth } from '../middlewares/authMiddleware';
 
 export const router = Router();
 
