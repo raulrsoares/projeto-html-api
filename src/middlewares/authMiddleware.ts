497853e46e5d22a5cd8ceb54f7fd5afbb5567e84
raulrsoares <raulrodriguessoares@gmail.com>
2025-04-12 03:31:54 -0300

    auth middleware, dotenv values

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,6 +1,11 @@
 import { Request, Response, NextFunction, RequestHandler } from 'express';
 import jwt from 'jsonwebtoken';
 import { UnauthorizedError } from './errorHandler';
+const base64Secret = process.env.JWT_SECRET_BASE64 as string;
+
+if (!base64Secret) {
+  throw new Error('JWT_SECRET_BASE64 não definido no .env');
+}
 
 export const jwtAuth: RequestHandler = (
   req: Request,
@@ -15,18 +20,14 @@ export const jwtAuth: RequestHandler = (
 
   const token = authHeader.split(' ')[1];
 
-  try {
-    const secret = process.env.JWT_SECRET;
+  const secret: jwt.Secret = Buffer.from(base64Secret, 'base64');
 
-    if (!secret) {
-      throw new UnauthorizedError('chave de decrepitação invalida');
-    }
-
-    const decoded = jwt.verify(token, secret);
-
-    (req as any).userInfo = decoded;
-    next();
-  } catch (error) {
-    next(error);
+  if (!secret) {
+    throw new UnauthorizedError('chave de decrepitação invalida');
   }
+
+  const decoded = jwt.verify(token, secret);
+
+  (req as any).userInfo = decoded;
+  next();
 };
