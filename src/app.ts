6ecf98ae89f2ecae214c1f5d26b7023a3f7c281c
raulrsoares <raulrodriguessoares@gmail.com>
2025-04-16 16:33:25 -0300

    cors

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 import express, { Express } from 'express';
+import cors, { CorsOptions } from 'cors';
 import morgan from 'morgan';
 import { router } from './routes/routes';
 import { errorHandler } from './middlewares/errorHandler';
@@ -7,6 +8,13 @@ import { timeLog } from './middlewares/timer.middlewares';
 
 const app: Express = express();
 
+const corsOptions: CorsOptions = {
+  optionsSuccessStatus: 200,
+  credentials: true,
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(timeLog);
 app.use(morgan('tiny'));
