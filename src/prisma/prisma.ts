4940b84ce9b76d7409a89371155f13776c16e620
raulrsoares <raulrodriguessoares@gmail.com>
2025-04-16 17:14:11 -0300

    prima out file and vercel

--- src/prisma/prisma.ts
+++ src/prisma/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '../../generated/prisma';
+import { PrismaClient } from './generated/prisma';
 
 const prismaClient = new PrismaClient({
   log: ['info', 'warn', 'error'],
